47420a5395638cc4be5dbecaa9936dcf6484c4bc
jasonbdt <ja.bladt@gmail.com>
2025-10-28 02:22:41 +0100

    Update another restaurant record

--- update.py
+++ update.py
@@ -7,3 +7,10 @@ restaurant_to_update = session.query(Restaurant) \
 
 restaurant_to_update.famous_dish = "French Toast and Bacon"
 session.commit()
+
+metropolitan = session.query(Restaurant) \
+    .filter(Restaurant.restaurant_name == "The Metropolitan") \
+    .one()
+
+metropolitan.restaurant_city = "Luxembourg"
+session.commit()
